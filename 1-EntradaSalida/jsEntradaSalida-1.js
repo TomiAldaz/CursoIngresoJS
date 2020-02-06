e210de9d4e90e1855336d3fcdc2fcb8330a6a873
Tomas Aldaz <60740250+TomiAldaz@users.noreply.github.com>
2020-02-06 11:12:49 -0300

    Primer Clase Ejercicio 1
    Aprendi banda

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,26 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	var primerPrecio;
+	var segundoPrecio;
+	var apellido="nadie";
+
+	apellido="Aldaz";
+	alert(apellido);
+	// Es un valor literal si esta entre ""
+	//alert(" Tal cual "); literal(sabemos q va a salir) o variable(no sabemos lo q va a salir)
+	//alert(apellido);
+	//alert("apellido");
+	//alert();//sale vacio
+	//alert(primerPreco) Si la variable no existe no podemos hacer nada
+	// F12-> Sources ->  Click en la linea de codigo donde esta la variable-> tocar "mostrar" -> F11 para pasar de codigo 
+
+
+
+
+
+
+
+
 }
 
