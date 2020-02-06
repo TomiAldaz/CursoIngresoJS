3fa2f7fd5627cbc1b24903c94031b8178f5d538e
Tomas Aldaz <60740250+TomiAldaz@users.noreply.github.com>
2020-02-06 12:12:03 -0300

    ejercicio 2
    casi

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,12 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	var nombre;
+
+	nombre = prompt("Please enter your name", "Harry Poter"); // reotrna algo. lo voy a tener q asignar a una variable EJ(var nombre = prompt() siempre el igual )
+	console.log ( nombre ); // reotrna algo. lo voy a tener q asignar a una variable EJ(var nombre = prompt() siempre el igual )
+	console.log("Coloque su nombre aqui " + nombre    );
+ 
+
 }
 
