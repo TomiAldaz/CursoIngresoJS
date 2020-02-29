34ac2c24938144186a2acc5f6f6aca39c752ae1e
alumno <alumno>
2020-02-29 15:23:50 -0300

    Hotel en E/S 2

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,20 +2,144 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombre;
+	/*var nombre;
 
 	nombre = prompt("Please enter your name", "Harry Poter"); // reotrna algo. lo voy a tener q asignar a una variable EJ(var nombre = prompt() siempre el igual )
 	console.log ( nombre ); // reotrna algo. lo voy a tener q asignar a una variable EJ(var nombre = prompt() siempre el igual )
 	console.log("Su nombre es: " + nombre    );
  // CONCATENAR= Unir un mensaje literal con una o mas variables 
+*/
+	/* 
+b)Para la gestión de un hotel,
+ingresar los siguientes datos validados de una reserva
+nombre del huésped
+cantidad de personas 
+cantidad de dia de estadia 
+forma de pago(efectivo , tarjeta o QR)
+
+informar el huésped que trajo más personas en una sola reserva.
+la cantidad de personas que se quedaron más días
+la forma de pago más utilizada.
+el promedio de cantidad de días por reserva*/
+
+
+	var nombre;
+	var cantidadPersonas;
+	var cantidadDias;
+	var formaDePago;
+	var respuesta;
+	var masPersonasUnaReserva;
+	var masPersonasUnaReservaHuesped;
+	var maximoDeDias;
+	var maximoDeDiasCantidadPersonas;
+	var contador;
+	var contadorReservas;
+	var contadorQR;
+	var contadorTarjeta;
+	var contadorEfectivo;
+	var acumuladorDeDias;
+	var promedio;
+	var formaPagoMasUsada;
+
+
+	respuesta="Si";
+	contadorReservas=0;
+	contadorEfectivo=0;
+	contadorTarjeta=0;
+	contadorQR=0;
+	acumuladorDeDias=0;
+
+	while(respuesta=="Si")
+	{
+		contador= contador++;
+
+
+		nombre=prompt("Ingrese su nombre ");
+		while(!IsNaN(nombre))
+		{
+			nombre=prompt("Ingrese su nombre ");
+		}
+
+		cantidadPersonas=prompt("Ingrese la cantidad de personas");
+		cantidadPersonas=parseInt(cantidadPersonas);
+		while(IsNaN(cantidadPersonas)|| cantidadPersonas<1)
+		{
+			cantidadPersonas=prompt("Ingrese la cantidad de personas");
+		cantidadPersonas=parseInt(cantidadPersonas);
+		}
+
+		cantidadDias=prompt("Ingrese la cantidad de dias");
+		cantidadDias=parseInt(cantidadDias);
+		while(IsNaN(cantidadDias)|| cantidadDias<1)
+		{
+			cantidadDias=prompt("Ingrese la cantidad de dias");
+		cantidadDias=parseInt(cantidadDias);
+		}
+
+		formaDePago=prompt("Ingrese su forma de pago");
+		while(!IsNaN(formaDePago)|| formaDePago!= "Efectivo"&& formaDePago!="Tarjeta"&&formaDePago!= "QR")
+		{
+			formaDePago=prompt("Ingrese su forma de pago");
+		}
+
+		if (cantidadPersonas>masPersonasUnaReserva || contadorReservas==1) 
+		{
+			cantidadPersonas=masPersonasUnaReserva;
+			masPersonasUnaReservaHuesped=nombre;
+			
+		}
+
+		if (cantidadDias>maximoDeDias|| contadorReservas==1) 
+		{
+			cantidadDias=maximoDeDias;
+			maximoDeDiasCantidadPersonas=cantidadPersonas;	
+			
+		}
+
+		switch(formaDePago)
+		{
+			case "Efectivo":
+			contadorEfectivo=contadorEfectivo++;
+				break;
+			case "Tarjeta":
+			contadorTarjeta++;
+				break;
+			default
+			contadorQR++;
+				break;
+
+		}
+
+		acumuladorDeDias=acumuladorDeDias+cantidadDias;
+
+	}
+
+	if(contadorEfectivo>contadorTarjeta && contadorEfectivo>contadorQR)
+	{
+		formaPagoMasUsada="Efectivo";
+		document.write("La forma de pago más utilizada fue el Efectivo");
+	}else
+	{
+		if (contadorTarjeta>contadorQR) 
+		{
+			formaPagoMasUsada="Tarjeta";
+			document.write("La forma de pago más utilizada fue la Tarjeta");
+
+		}else
+		{
+			formaPagoMasUsada="QR";
+			document.write("La forma de pago más utilizada fue el QR");
+
+		}
+	}
 
 
 
+	promedio=cantidadDias/contadorReservas;
 
-
-
-
-
+	document.write("El huésped que trajo más personas en una sola reserva fue " + masPersonasUnaReservaHuesped);
+	document.write("La cantidad de personas que se quedaron más días fue " + cantidadPersonasMasDias);
+	
 
 
 
